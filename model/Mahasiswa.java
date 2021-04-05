d86f0ee95a26ac06607fdabc7daeb7b3a6ab8816
rudi426 <81942124+rudi426@users.noreply.github.com>
2021-04-05 21:41:15 +0800

    commit

---
+++ model/Mahasiswa.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Mahasiswa {
+  public static void tampilanInfo() {
+    System.out.println();
+    System.out.println("---------------------------------------");
+    System.out.println("|           Data Mahasiswa            |");
+    System.out.println("|-------------------------------------|");
+    System.out.println("| NPM  : 19630937                     |");
+    System.out.println("| Nama : Muhammad Rudi Maulana        |");
+    System.out.println("---------------------------------------");
+  }
+}
---
+++ model/MataKuliah.java
@@ -0,0 +1,13 @@
+package model;
+
+public class MataKuliah {
+  public static void tampilanInfo() {
+    System.out.println();
+    System.out.println("---------------------------------------");
+    System.out.println("|          Data Mata Kuliah           |");
+    System.out.println("|-------------------------------------|");
+    System.out.println("| Kode : TIF3402                      |");
+    System.out.println("| Nama : Pemrograman Berbasis Objek 1 |");
+    System.out.println("---------------------------------------");
+  }
+}
